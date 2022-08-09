4bf179e3d57d3dfb2bc3cbd8264f9aff95c35f03
AndrewCLu <andrewclu98@gmail.com>
2022-08-09 14:33:49 -0400

    add proving capability to generate witness

--- generate_witness.sh
+++ generate_witness.sh
@@ -1,29 +1,41 @@
 #!/bin/sh
-# Usage: ./generate_witness.sh [circuit_name] [test_mode] [input]
+# Usage: ./generate_witness.sh [circuit_name] [test_mode] [generate_proof] [input]
 
 CIRCUIT=$1
 TEST_FLAG=$2
-INPUT=$3
-if [ $TEST_FLAG == "true" ]
-then
+PROOF_FLAG=$3
+INPUT=$4
+
+if [ $TEST_FLAG == "true" ]; then
   TEST=true
-elif [ $TEST_FLAG == "false" ]
-then
+elif [ $TEST_FLAG == "false" ];then
   TEST=false
 else
   echo "Test mode must be set to either true or false!"
   exit
 fi
 
-echo "Generating witness for $CIRCUIT, test mode: $TEST";
+if [ $PROOF_FLAG == "true" ]; then
+  PROOF=true
+elif [ $PROOF_FLAG == "false" ]; then
+  PROOF=false
+else
+  echo "Proof mode must be set to either true or false!"
+  exit
+fi
 
-if [ $TEST = false ]
-then
+echo "Generating witness for $CIRCUIT, test mode: $TEST, proof mode: $PROOF";
+
+if [ $TEST = false ]; then
   cd generated/$CIRCUIT;
   echo $INPUT > input.json;
   node ${CIRCUIT}_js/generate_witness.js ${CIRCUIT}_js/$CIRCUIT.wasm input.json witness.wtns
+  if [ $PROOF = true ]; then
+    snarkjs plonk prove ${CIRCUIT}_final.zkey witness.wtns proof.json public.json;
 else
   cd test_generated/$CIRCUIT;
   echo $INPUT > input.json;
   node ${CIRCUIT}_js/generate_witness.js ${CIRCUIT}_js/$CIRCUIT.wasm input.json witness.wtns
+  if [ $PROOF = true ]; then
+    snarkjs plonk prove ${CIRCUIT}_final.zkey witness.wtns proof.json public.json;
 fi
