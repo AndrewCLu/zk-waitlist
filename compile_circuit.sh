a5f7ec1caa3d6d6141900ea791f1012cd15077ca
AndrewCLu <andrewclu98@gmail.com>
2022-08-09 23:31:29 -0400

    add zkey generation to compilation

--- compile_circuit.sh
+++ compile_circuit.sh
@@ -23,10 +23,14 @@ then
   circom ../../circuits/$CIRCUIT.circom --r1cs --wasm --sym;
   snarkjs r1cs info $CIRCUIT.r1cs;
   snarkjs r1cs export json $CIRCUIT.r1cs $CIRCUIT.r1cs.json;
+  snarkjs plonk setup $CIRCUIT.r1cs ../../ptau/powersOfTau28_hez_final_15.ptau ${CIRCUIT}_final.zkey
+  snarkjs zkey export verificationkey ${CIRCUIT}_final.zkey verification_key.json
 else
   mkdir test_generated/$CIRCUIT;
   cd test_generated/$CIRCUIT;
   circom ../../test_circuits/$CIRCUIT.circom --r1cs --wasm --sym;
   snarkjs r1cs info $CIRCUIT.r1cs;
   snarkjs r1cs export json $CIRCUIT.r1cs $CIRCUIT.r1cs.json;
+  snarkjs plonk setup $CIRCUIT.r1cs ../../ptau/powersOfTau28_hez_final_15.ptau ${CIRCUIT}_final.zkey
+  snarkjs zkey export verificationkey ${CIRCUIT}_final.zkey verification_key.json
 fi
