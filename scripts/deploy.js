010b6d72b8641c4ce74b03d4fc725278cafcf68b
AndrewCLu <andrewclu98@gmail.com>
2022-08-10 23:52:55 -0400

    add deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,19 @@
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  const VerifierContract = await ethers.getContractFactory("RedeemerVerifier");
+  const verifier = await VerifierContract.deploy();
+
+  console.log("Verifier address:", verifier.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
