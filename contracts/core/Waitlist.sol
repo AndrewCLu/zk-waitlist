fb260861b57e9e8047a11b1de652f9466e38979e
AndrewCLu <andrewclu98@gmail.com>
2022-08-13 18:04:16 -0400

    add lock params to waitlist

--- contracts/core/Waitlist.sol
+++ contracts/core/Waitlist.sol
@@ -15,17 +15,29 @@ contract Waitlist is IWaitlist {
   // List of public commitments from waitlisted users
   uint[] commitments;
 
+  // Whether or not the waitlist has been locked
+  bool isLocked;
+
+  // Root of the merkle tree
+  uint merkleRoot;
+
   // Mapping of nullifiers that have been used
   mapping(uint => bool) internal usedNullifiers;
 
   constructor(uint8 _maxWaitlistSpots) {
     maxWaitlistSpots = _maxWaitlistSpots;
     usedWaitlistSpots = 0;
+    isLocked = false;
   }
 
   function join(uint commitment) public returns (bool) {
-    if (waitlistedUsers[msg.sender]) {
+    // Waitlist is locked
+    if (isLocked) {
       return false;
+    // User is already on waitlist
+    } else if (waitlistedUsers[msg.sender]) {
+      return false;
+    // Waitlist is full
     } else if (usedWaitlistSpots >= maxWaitlistSpots) {
       return false;
     } else {
@@ -35,6 +47,13 @@ contract Waitlist is IWaitlist {
       commitments.push(commitment);
       return true;
     }
+  }
+
+  function lock(
+    bytes memory proof, 
+    uint[] memory pubSignals
+  ) public returns (bool) {
+    return true;
   }
 
   function redeem(
